98971bbb4084907b571df5053fd5643340590f63
FirelordOzai47 <mustafadgn.4@gmail.com>
2023-03-02 22:59:12 +0300

    GET,POST,PUT,DELETE Metodlarının İmplementosyanları

--- DemoWebApi/App_Start/WebApiConfig.cs
+++ DemoWebApi/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using Newtonsoft.Json.Serialization;
 
 namespace DemoWebApi
 {
@@ -17,8 +18,12 @@ namespace DemoWebApi
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
+                defaults: new { id = RouteParameter.Optional } 
             );
+            config.Formatters.JsonFormatter.SerializerSettings.Formatting = Newtonsoft.Json.Formatting.Indented;
+            ////config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            //config.Formatters.Remove(config.Formatters.XmlFormatter);
+            //config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new System.Net.Http.Headers.MediaTypeHeaderValue("text/html"));
         }
     }
 }
--- DemoWebApi/Controllers/EmployeesController.cs
+++ DemoWebApi/Controllers/EmployeesController.cs
@@ -18,13 +18,120 @@ namespace DemoWebApi.Controllers
             }
 
         }
-        public Employees Get(int id)
+        public HttpResponseMessage Get(int id)
         {
             using (EmployeeDBEntities entities = new EmployeeDBEntities())
             {
-                return entities.Employees.FirstOrDefault(e=> e.ID==id);
+                var entity = entities.Employees.FirstOrDefault(e => e.ID == id);
+                if (entity != null)
+                {
+                    return Request.CreateResponse(HttpStatusCode.OK, entity);
+                }
+                else
+                {
+
+                    return Request.CreateResponse(HttpStatusCode.NotFound, "Employee with ID=  " + id.ToString() + "Not Found");
+                }
             }
 
         }
+        public HttpResponseMessage Post([FromBody] Employees employees)
+        {
+            try
+            {
+                using (EmployeeDBEntities entities = new EmployeeDBEntities())
+                {
+
+                    entities.Employees.Add(employees);
+                    entities.SaveChanges();
+
+                    var message = Request.CreateResponse(HttpStatusCode.Created, employees);
+                    message.Headers.Location = new Uri(Request.RequestUri + employees.ID.ToString());
+                    return message;
+                }
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+
+            }
+
+
+        }
+
+        public HttpResponseMessage Deleted(int id)
+        {
+            try
+            {
+                using (EmployeeDBEntities entities = new EmployeeDBEntities())
+                {
+
+                    var entity = entities.Employees.FirstOrDefault(e => e.ID == id);
+                    if (entity == null)
+                    {
+                        return Request.CreateResponse(HttpStatusCode.NotFound, "Employee with ID=  " + id.ToString() + "Not Found to delete");
+
+                    }
+                    else
+                    {
+                        entities.Employees.Remove(entity);
+                        entities.SaveChanges();
+                        return Request.CreateResponse(HttpStatusCode.OK);
+
+
+                    }
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+
+        }
+        public HttpResponseMessage Put(int id, [FromBody] Employees employees)
+        {
+            try
+            {
+                using (EmployeeDBEntities entities = new EmployeeDBEntities())
+                {
+                    var entity = entities.Employees.FirstOrDefault(e => e.ID == id);
+
+                    if (entity == null)
+                    {
+                        return Request.CreateResponse(HttpStatusCode.NotFound, "Employee with ID=  " + id.ToString() + "Not Found to delete");
+
+                    }
+                    else
+                    {
+                        entity.FirstName = employees.FirstName;
+                        entity.LastName = employees.LastName;
+                        entity.Gender = employees.Gender;
+                        entity.Salary = employees.Salary;
+                        entities.SaveChanges();
+                        return Request.CreateResponse(HttpStatusCode.OK, entity);
+
+
+                    }
+
+
+
+                }
+            }
+
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+
+            }
+
+
+
+        }
     }
-}
+         
+
+} 
+
